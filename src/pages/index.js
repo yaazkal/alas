0ad9ea493211f15dc9ae1c4b8ab4575b4605ce4b
Yaazkal <yaazkal@gmail.com>
2019-01-10 21:16:35 -0500

    Oculta video de YT en página principal en los móviles

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,7 +74,7 @@ const IndexPage = () => (
       <HeroBody>
         <Container>
           <Columns>
-            <Column>
+            <Column isHidden="mobile">
               <iframe
                 title="Titanes Caracol Medellín"
                 width="560"
