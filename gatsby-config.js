ed186d68b4fda16f5cd9a4a0b9f175dd6586b062
Yaazkal <yaazkal@gmail.com>
2019-01-11 22:07:49 -0500

    Cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `alas`,
+        short_name: `alas`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
